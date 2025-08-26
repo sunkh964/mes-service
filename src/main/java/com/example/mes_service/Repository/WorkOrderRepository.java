5b7bd44249c6bcbc641f12bd7f92669e766b1417
MallangDanbi <2122204@donga.ac.kr>
2025-08-26 12:27:34 +0900

    마무리. 추가할거 아닌 이상 손 댈것 없음

--- src/main/java/com/example/mes_service/Repository/WorkOrderRepository.java
+++ src/main/java/com/example/mes_service/Repository/WorkOrderRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WorkOrderRepository extends JpaRepository<WorkOrder, Integer> {
+    long countByCurrentStatus(String currentStatus);
 }
